9addc611fdf621335fd001b5c65e56479d319d3e
Om_Rajput <omrajputofficial07@gmail.com>
2025-08-04 16:07:42 +0530

    Update calculate_electricitybill.py

--- F.B.S/Core Python/Assignments/Assignment 3/calculate_electricitybill.py
+++ F.B.S/Core Python/Assignments/Assignment 3/calculate_electricitybill.py
@@ -3,10 +3,12 @@ total_bill = 0
 if(unit>0):
     if(unit>50):
         if(unit>150):
-            if(unit>200):
-                pass
+            if(unit>250):
+                total_bill = 50 * 0.50 + 100 * 0.75 + 100 * 1.20
+                remaining_unit = unit - 250
+                total_bill = total_bill + remaining_unit * 1.50
             else:
-                total_bill = 50 * 0.50
+                total_bill = 50 * 0.50 + 100 * 0.75
                 remaining_unit = unit - 150
                 total_bill = total_bill + (remaining_unit * 1.20)
         else:
@@ -17,6 +19,9 @@ if(unit>0):
         total_bill = unit * 0.50
 else:
     print("the input value is Invalid..")
+
+surcharge = total_bill * 20/100
+total_bill = total_bill + surcharge 
 
 print(total_bill)
 
